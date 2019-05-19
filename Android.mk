eae36f6549ebe590316bad401c890209c1bd138c
XiNGRZ <chenxingyu92@gmail.com>
2019-05-20 00:49:46 +0800

    Revert "display-hals: Expose c2d2.h even if TARGET_DISABLE_DISPLAY is set"
    This reverts commit 3749ad5ed6f5962fe3c3b061f86073d43acfa1e9.
    
    Change-Id: I7b1a5150647ff280fdaa78c77077b1a1b4afe9a6

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ sdm-libs := sdm/libs
 display-hals := include libdebug $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils
 endif
 
@@ -15,11 +15,6 @@ display-hals += commonsys-intf/libdisplayconfig
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
     display-hals += liblight
 endif
-else
-ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals := libcopybit
-endif
-endif #TARGET_DISABLE_DISPLAY
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
@@ -28,5 +23,6 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
+endif #TARGET_DISABLE_DISPLAY
 
 endif
